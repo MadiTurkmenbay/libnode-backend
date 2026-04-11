174a2409cdb94ee23f90de18b0c422eb11f5aa67
Madi <poyyyg@gmail.com>
2026-04-11 23:44:39 +0500

    feat: configure CORS and final backend adjustments

--- Program.cs
+++ Program.cs
@@ -6,6 +6,16 @@ var builder = WebApplication.CreateBuilder(args);
 
 // ── Services ────────────────────────────────────────────────────────────────
 
+// CORS (Разрешаем запросы с фронтенда)
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
 // DbContext → PostgreSQL
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
@@ -43,6 +53,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseCors("AllowAll");
 app.UseAuthorization();
 app.MapControllers();
 
